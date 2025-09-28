1eb8f6a78ae9b341a2091c12b6acbab03d038521
kingm6652-cell <kingm6652@student.faytechcc.edu>
2025-09-28 19:59:46 -0400

    Add files via upload

---
+++ P1LAB_KingMaria.py
@@ -0,0 +1,10 @@
+# Maria King
+# 9/28?25
+# P1LAB
+# Description: Get input from user and get an output.
+
+first_name = input("Enter your first name: ")
+last_name = input("Enter your last name: ")
+print(f"Hello, {first_name} {last_name}! Welcome to CTI-110")
+
+
